03edfd7c79af1d681e59a6bef5363c72b28a6a32
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 02:12:31 -0700

    Chap7 Lab2

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+# Import randomizer
+import random
 
-# ******************************
-# Make your Code
-# ******************************
+# Create list called numbers
+numbers = []
+
+# Get 10 int values and add to list
+i = 0
+while i < 10:
+    num = random.randint(0, 100)
+    numbers.append(num)
+    i += 1
+
+# Print smallest value in list and its index
+print("List of random numbers:", numbers)
+print("Smallest value:", min(numbers))
+print("Index of", min(numbers), "is", numbers.index(min(numbers)))
